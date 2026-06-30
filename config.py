0f4248e29a3f126561704025c9431fcda7b4dfb7
Prajwal jotiram Sankpal <prajwalsankpal7896@gmail.com>
2026-06-30 16:55:47 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = "agro_sage_secret_key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "agro_sage.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
